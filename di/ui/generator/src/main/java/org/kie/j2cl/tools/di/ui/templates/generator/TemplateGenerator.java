2141ed7f1f3553cc19f66172a9f8f8905e10ea08
Dmitrii Tikhomirov <chani.liet@gmail.com>
2025-11-03 20:12:23 -0800

    backport: Multi-line class attributes in HTML templates generate invalid Java class
    Signed-off-by: Dmitrii Tikhomirov <chani.liet@gmail.com>

--- di/ui/generator/src/main/java/org/kie/j2cl/tools/di/ui/templates/generator/TemplateGenerator.java
+++ di/ui/generator/src/main/java/org/kie/j2cl/tools/di/ui/templates/generator/TemplateGenerator.java
@@ -428,7 +428,7 @@ public class TemplateGenerator extends IOCGenerator<BeanDefinition> {
                                TemplateDefinition templateDefinition) {
         templateContext.getRoot().getAttributes().stream()
                 .map(attr -> new org.kie.j2cl.tools.di.ui.templates.generator.dto.Attribute(attr.getKey(),
-                        attr.getValue()))
+                        attr.getValue().replaceAll("\\s+", " ").trim()))
                 .forEach(a -> templateDefinition.getAttributes().add(a));
     }
 
