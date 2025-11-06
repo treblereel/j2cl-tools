7b9185b97c3f502d763096b683862e6a8e68d83e
Dmitrii Tikhomirov <chani.liet@gmail.com>
2025-11-05 21:03:19 -0800

    skip built-in type properties for classes not annotated with @YAMLMapper
    Signed-off-by: Dmitrii Tikhomirov <chani.liet@gmail.com>

--- mapper-yaml/processor/src/main/java/org/kie/j2cl/tools/yaml/mapper/processor/BeanProcessor.java
+++ mapper-yaml/processor/src/main/java/org/kie/j2cl/tools/yaml/mapper/processor/BeanProcessor.java
@@ -69,7 +69,9 @@ public class BeanProcessor {
   }
 
   private void processBean(TypeElement bean) {
-    if (!(beans.contains(bean) || typeUtils.isObject(bean.asType()))) {
+    if (!(beans.contains(bean)
+        || typeUtils.isObject(bean.asType())
+        || context.getTypeRegistry().containsSerializer(bean.asType()))) {
       beans.add(checkBean(bean));
       context.getTypeUtils().getAllFieldsIn(bean).stream()
           .filter(field -> field.getAnnotation(YamlTransient.class) == null)
@@ -100,7 +102,7 @@ public class BeanProcessor {
           }
         }
         return;
-      } else if (MoreTypes.isType(type)) {
+      } else if (MoreTypes.isType(type) && !context.getTypeUtils().isSimpleType(type)) {
         if (!MoreTypes.asElement(type).getKind().equals(ElementKind.ENUM)) {
           processBean(typeUtils.toTypeElement(type));
         }
